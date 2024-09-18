57a8739c4e460bc11d143ae33c5f2fbd2ba8e645
CristianTambaco <168015193+CristianTambaco@users.noreply.github.com>
2024-09-18 10:04:26 -0500

    Add files via upload

---
+++ Matriz arreglos  de dos dimensiones.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+void semilla(){
+	srand(time(0));
+}
+
+int numaleatorio(){
+	int aleatorio=rand()%99+1;
+	return aleatorio;
+}
+
+
+void sumamatriz(){
+	int size;
+	cout<<"*** Matrices con numeros aleatorios ***"<<endl;
+	cout<<"                                          "<<endl;
+	cout<<"Ingrese el tamanio de la matriz: "; cin>>size;
+	int matriz1[size][size];
+	int matriz2[size][size];
+	int matrizsuma[size][size];
+	
+	for (int i=0; i<size; i++){
+		for( int j=0; j<size ;j++){
+			matriz1[i][j]=numaleatorio();
+			matriz2[i][j]=numaleatorio();
+		}
+	}
+	
+	
+	//matriz
+	cout<<"Matriz 1"<<endl;
+	for (int i=0; i<size; i++){
+		for( int j=0; j<size ;j++){
+			cout<<matriz1[i][j]<<"\t";
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+	//matriz 2
+	cout<<"Matriz 2"<<endl;
+	for (int i=0; i<size; i++){
+		for( int j=0; j<size ;j++){
+			cout<< matriz2[i][j]<<"\t";
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+	
+	cout<<"Matriz Suma"<<endl;
+	for (int i=0; i<size; i++){
+		for(int j=0; j<size; j++){
+			matrizsuma[i][j]=matriz1[i][j]+matriz2[i][j];
+			cout<<matrizsuma[i][j]<<"\t";
+		}
+		cout<<endl;
+	}
+}
+
+int main(){
+	semilla();
+	sumamatriz();
+	return 0;
+}
